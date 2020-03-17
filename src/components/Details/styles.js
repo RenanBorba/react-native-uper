d061e894e169a82a12e10b0538b73f572559e11c
Renan Borba <48495838+RenanBorba@users.noreply.github.com>
2020-03-17 10:30:55 -0300

    Update styles.js

--- src/components/Details/styles.js
+++ src/components/Details/styles.js
@@ -13,22 +13,22 @@ export const Container = styled.View`
   elevation: 3;
   border: 1px solid #DDD;
   align-items: center;
-  padding: 20px;
+  padding: 20px
 `;
 
 export const TypeTitle = styled.Text`
   font-size: 20px;
-  color: #222;
+  color: #222
 `;
 
 export const TypeDescription = styled.Text`
   color: #666;
-  font-size: 14px;
+  font-size: 14px
 `;
 
 export const TypeImage = styled.Image`
   height: 80px;
-  margin: 10px 0;
+  margin: 10px 0
 `;
 
 export const RequestButton = styled.TouchableOpacity`
@@ -37,11 +37,11 @@ export const RequestButton = styled.TouchableOpacity`
   align-items: center;
   height: 44px;
   align-self: stretch;
-  margin-top: 10px;
+  margin-top: 10px
 `;
 
 export const RequestButtonText = styled.Text`
   color: #FFF;
   font-weight: bold;
-  font-size: 18px;
+  font-size: 18px
 `;
